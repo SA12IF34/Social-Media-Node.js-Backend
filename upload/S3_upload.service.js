9be7e26cd10cae7a494a2e5242ce2e209a5e945f
Saif <ayeshsaif367@gmail.com>
2025-09-10 15:10:41 +0300

    Updated environment variables for AWS

--- upload/S3_upload.service.js
+++ upload/S3_upload.service.js
@@ -2,7 +2,11 @@ const AWS = require('aws-sdk');
 const getStream = require('./Stream.service');
 AWS.config.update({region: process.env.AWS_S3_BUCKET_REGION})
 
-const s3 = new AWS.S3()
+const s3 = new AWS.S3({
+    region: process.env.MY_AWS_REGION,
+    accessKeyId: process.env.MY_AWS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_KEY
+})
 
 
 const handleUpload = async (file, fileName, fileType) => {
